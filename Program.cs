93bb5779b5329f6351f758f0bb18db8f2cffb864
kyllian18 <64402572+kyllian18@users.noreply.github.com>
2021-10-20 15:46:30 +0100

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace readfile
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string path = "..";
+            string file = "tux.txt";
+            string fileToRead = $"{path}/{file}";
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(fileToRead))
+                {
+                    Console.Write($"Reading {fileToRead}");
+
+                    string line;
+
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine($"\t{line}");
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read:");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
